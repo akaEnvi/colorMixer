b99eb84db94b64bcb6c4a089a32b585a55bbc6f8
akaEnvi <envimth@gmail.com>
2022-09-03 14:32:34 +0200

    fix: Head-bar

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ function changeColor() {
 document.querySelector("#slider-red").addEventListener("input", changeColor);
 document.querySelector("#slider-green").addEventListener("input", changeColor);
 document.querySelector("#slider-blue").addEventListener("input", changeColor);
+
+const color = document.querySelector("colorOutput");
+
+/*function ColorToHex(color) {
+  let hexadecimal = color.toSTring(16);
+  return hexadecimal.lenght == 1 ? "0" + hexerdecimal : hexadecimal;
+}
+
+function ConvertRGBtoHex(red, green, blue) {
+  return "#" + ColorToHex(red) + ColorToHex(green) + ColorToHex(blue);
+}
+console.log(convertRGBtoHex(255, 100, 200));
